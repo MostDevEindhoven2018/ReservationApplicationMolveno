2c3ac1fb125295301526021d1d2364df165b3ee3
Xiang-jong Hu <xiang_jonghu@hotmail.com>
2018-04-10 16:08:27 +0200

    Form

--- ReservationApplicationMolveno/Program.cs
+++ ReservationApplicationMolveno/Program.cs
@@ -16,7 +16,7 @@ namespace ReservationApplicationMolveno
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formReservation());
         }
     }
 }
