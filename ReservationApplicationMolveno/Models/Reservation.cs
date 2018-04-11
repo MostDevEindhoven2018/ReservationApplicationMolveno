1cf6dfdf1c568a0ace0398a65e6e04f7b15db297
George II <georgevassiliadis@hotmail.com>
2018-04-11 21:49:57 +0200

    Added models and minor changes in form.

---
+++ ReservationApplicationMolveno/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data 
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; } //automatically generated
+        public int PartySize { get; set; }
+        public Guest Guest { get; set; }
+        public Table Table { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ ReservationApplicationMolveno/ReservationForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReservationApplicationMolveno
+{
+    public partial class ReservationForm : Form
+    {
+        public ReservationForm()
+        {
+            InitializeComponent();
+
+            // Adds 0 till 23 hours to the combobox with the reservation time
+            for (int i = 0; i <= 23; i++)
+            {
+                if (i < 10)
+                {
+                    inputBeginTimeHour.Items.Add("0" + i);
+                }
+                else
+                {
+                    inputBeginTimeHour.Items.Add(i);
+                }
+
+            }
+
+            // Adds 0 till 59 to the combobox with the reservation time
+            for (int i = 0; i < 60; i += 15)
+            {
+                if (i == 0)
+                {
+                    inputBeginTimeMinute.Items.Add("00");
+                }
+                else
+                {
+                    inputBeginTimeMinute.Items.Add(i);
+                }
+            }
+        }
+
+        private void bt_reserve_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
